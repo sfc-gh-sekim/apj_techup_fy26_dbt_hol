9e9c56a9da16a8709e62f83a3b2684925210645a
seankimthesnowflake <sean.kim@snowflake.com>
2025-10-22 06:34:40 +1100

    clean up logic

--- models/silver/taxi_availability.sql
+++ models/silver/taxi_availability.sql
@@ -1,5 +1,6 @@
 select 
-    ta.DATA:features[0]:properties:timestamp::timestamp as timestamp, 
-    st_makepoint(f.value[0], f.value[1]) as taxi_coords
+    dateadd(hour, 8, retrieved_at) as timestamp_sgt
+    , ta.DATA:features[0]:properties:timestamp::timestamp as timestamp
+    , st_makepoint(f.value[0], f.value[1]) as taxi_coords
 from {{ source('raw_data', 'taxi_availability') }} ta,
 lateral flatten(input => ta.DATA:features[0]:geometry:coordinates) f
--- models/silver/tourist_attractions.sql
+++ models/silver/tourist_attractions.sql
@@ -1,14 +1,14 @@
 SELECT 
-    REGEXP_SUBSTR(f.value:properties:Description::varchar, '<th>URL_PATH</th>\\s*<td>([^<]+)</td>', 1, 1, 'e', 1) AS url_path,
-    REGEXP_SUBSTR(f.value:properties:Description::varchar, '<th>ADDRESS</th>\\s*<td>([^<]+)</td>', 1, 1, 'e', 1) AS address,
-    REGEXP_SUBSTR(f.value:properties:Description::varchar, '<th>PAGETITLE</th>\\s*<td>([^<]+)</td>', 1, 1, 'e', 1) AS page_title,
-    REGEXP_SUBSTR(f.value:properties:Description::varchar, '<th>IMAGE_PATH</th>\\s*<td>([^<]+)</td>', 1, 1, 'e', 1) AS image_path,
-    REGEXP_SUBSTR(f.value:properties:Description::varchar, '<th>PHOTOCREDITS</th>\\s*<td>([^<]+)</td>', 1, 1, 'e', 1) AS photo_credits,
-    REGEXP_SUBSTR(f.value:properties:Description::varchar, '<th>OVERVIEW</th>\\s*<td>([^<]+)</td>', 1, 1, 'e', 1) AS overview,
-    REGEXP_SUBSTR(f.value:properties:Description::varchar, '<th>EXTERNAL_LINK</th>\\s*<td>([^<]+)</td>', 1, 1, 'e', 1) AS external_link,
-    REGEXP_SUBSTR(f.value:properties:Description::varchar, '<th>OPENING_HOURS</th>\\s*<td>([^<]+)</td>', 1, 1, 'e', 1) AS opening_hours,
-    st_makepoint(f.value:geometry:coordinates[0], f.value:geometry:coordinates[1]) as coords,
-    f.value:properties:Description::varchar as raw_description_html
+    dateadd(hour, 8, retrieved_at) as timestamp_sgt
+    , REGEXP_SUBSTR(f.value:properties:Description::varchar, '<th>URL_PATH</th>\\s*<td>([^<]+)</td>', 1, 1, 'e', 1) AS url_path
+    , REGEXP_SUBSTR(f.value:properties:Description::varchar, '<th>ADDRESS</th>\\s*<td>([^<]+)</td>', 1, 1, 'e', 1) AS address
+    , REGEXP_SUBSTR(f.value:properties:Description::varchar, '<th>PAGETITLE</th>\\s*<td>([^<]+)</td>', 1, 1, 'e', 1) AS page_title
+    , REGEXP_SUBSTR(f.value:properties:Description::varchar, '<th>IMAGE_PATH</th>\\s*<td>([^<]+)</td>', 1, 1, 'e', 1) AS image_path
+    , REGEXP_SUBSTR(f.value:properties:Description::varchar, '<th>PHOTOCREDITS</th>\\s*<td>([^<]+)</td>', 1, 1, 'e', 1) AS photo_credits
+    , REGEXP_SUBSTR(f.value:properties:Description::varchar, '<th>OVERVIEW</th>\\s*<td>([^<]+)</td>', 1, 1, 'e', 1) AS overview
+    , REGEXP_SUBSTR(f.value:properties:Description::varchar, '<th>EXTERNAL_LINK</th>\\s*<td>([^<]+)</td>', 1, 1, 'e', 1) AS external_link
+    , REGEXP_SUBSTR(f.value:properties:Description::varchar, '<th>OPENING_HOURS</th>\\s*<td>([^<]+)</td>', 1, 1, 'e', 1) AS opening_hours
+    , st_makepoint(f.value:geometry:coordinates[0], f.value:geometry:coordinates[1]) as coords,
+    , f.value:properties:Description::varchar as raw_description_html
 from {{ source('raw_data', 'tourist_attractions') }} ta,
 lateral flatten(input => ta.DATA:features) f
-qualify rank() over (order by retrieved_at desc) = 1
